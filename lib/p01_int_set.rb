aa788673586cc0458c47924a8ba7911862bd1156
Philip Lowe <philip.lowe94@gmail.com>
2021-03-22 16:14:43 -0400

    Phase 1 complete

---
+++ lib/p01_int_set.rb
@@ -0,0 +1,113 @@
+class MaxIntSet
+
+  attr_reader :max
+  attr_accessor :store
+
+  def initialize(max)
+    @max = max
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    is_valid?(num)
+    @store[num] = true
+
+  end
+
+  def remove(num)
+    is_valid?(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    return @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+    raise 'Out of bounds' if num < 0 || num > max
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    self[num] << num if !include?(num) 
+  end
+
+  def remove(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    @store[num % num_buckets]
+  end 
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    if !include?(num)
+      self[num] << num 
+      @count += 1
+      resize! if @count == num_buckets
+    end
+  end
+
+  def remove(num)
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    @resize = Array.new(num_buckets) { Array.new }
+    @store.concat(@resize)
+    @store.each do |bucket|
+      bucket.each do |ele|
+        bucket.delete(ele)
+        @store[ele] << ele
+      end
+    end
+  end
+end
